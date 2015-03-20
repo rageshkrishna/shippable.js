b010325cdf2665edd79cfbbf9e97ff0d5aff5110
Ragesh Krishna <ragesh@rageshkrishna.com>
2015-03-20 03:40:36 -0400

    Remove info logs

--- requestHelper.js
+++ requestHelper.js
@@ -20,7 +20,7 @@ function RequestHelper(apiEndpoint, apiToken, log) {
     opts = _merge(opts);
     request[opts.method](opts, function(err, res, body) {
       if (!err) {
-        log.info('API', opts.method, opts.url, 'returned', res.statusCode);
+        log.debug('API', opts.method, opts.url, 'returned', res.statusCode);
         if (res.statusCode > 299) {
           err = res.statusCode;
         }
